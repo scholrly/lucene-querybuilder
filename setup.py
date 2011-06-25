46fbd4c73732b59bedd74df38142260ad7dc05c5
Matt Luongo <mhluongo@gmail.com>
2011-06-25 17:58:11 -0400

    Updated the setup.py to reflect the version change.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
 
 setup(
     name='lucene-querybuilder',
-    version='0.1.1',
+    version='0.1.2',
     author='Edd Purcell',
     author_email='epurcell3@gatech.edu',
     maintainer='Matt Luongo',
