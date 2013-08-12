137922e1f02276d3cd364d5fc876588d54cadc93
Matt Luongo <mhluongo@gmail.com>
2013-08-12 10:36:52 -0400

    Bumped the version to 0.2.
    Closes #18.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
 
 setup(
     name='lucene-querybuilder',
-    version='0.1.6',
+    version='0.2',
     author='Edd Purcell',
     author_email='epurcell3@gatech.edu',
     maintainer='Matt Luongo',
